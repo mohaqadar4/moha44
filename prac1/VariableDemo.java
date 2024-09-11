c3ed9b809b3a97d38030a963934dcfc7e3bc0120
mohaqadar4 <mqadar778@gmail.com>
2024-09-11 08:11:20 +0300

    Initial commit

--- prac1/VariableDemo.java
+++ prac1/VariableDemo.java
@@ -1,6 +1,5 @@
 public class VariableDemo {
 
-
     private static int staticCounter = 0;
 
     private int instanceCounter = 0;
@@ -12,11 +11,9 @@ public class VariableDemo {
     }
 
     public void demonstrateInstanceVariable() {
-
         instanceCounter++;
         System.out.println("Value of instanceCounter: " + instanceCounter);
     }
-
 
     public static void demonstrateStaticVariable() {
         staticCounter++;
@@ -24,40 +21,19 @@ public class VariableDemo {
     }
 
     public static void main(String[] args) {
-
         VariableDemo demo1 = new VariableDemo();
         VariableDemo demo2 = new VariableDemo();
 
-
         System.out.println("Demonstrating local variable:");
-        demo1.demonstrateLocalVariable();
-        demo2.demonstrateLocalVariable();
-
+        demo1.demonstrateLocalVariable(); 
+        demo2.demonstrateLocalVariable(); 
         System.out.println("\nDemonstrating instance variable:");
-        demo1.demonstrateInstanceVariable();
+        demo1.demonstrateInstanceVariable(); 
         demo2.demonstrateInstanceVariable();
 
-
+       
         System.out.println("\nDemonstrating static variable:");
-        VariableDemo.demonstrateStaticVariable();
-        VariableDemo.demonstrateStaticVariable();
-    }
-}
-public class Variable {
-    int instanceVar = 10;
-    
-    static int staticVar = 20;
-    
-    public void showVariables() {
-        int localVar = 30;
-        
-        System.out.println("Local Variable: " + localVar);
-        System.out.println("Instance Variable: " + instanceVar);
-        System.out.println("Static Variable: " + staticVar);
-    }
-    
-    public static void main(String[] args) {
-        VariableDemo demo = new VariableDemo();
-        demo.showVariables();
+        VariableDemo.demonstrateStaticVariable(); 
+        VariableDemo.demonstrateStaticVariable(); 
     }
 }
